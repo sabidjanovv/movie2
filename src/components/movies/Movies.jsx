a1f6ed2b18c1471d8779ff083d4dff225f9965d6
sabidjanovv <sabidjanov@gmail.com>
2024-12-25 10:18:21 +0500

    Grid cols 2

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -3,7 +3,7 @@ import MovieItem from "./MovieItem";
 
 const Movies = ({ data, bg }) => {
   return (
-    <div className="dark:bg-black container py-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="dark:bg-black container py-14 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {data?.map((movie) => (
         <MovieItem key={movie?.id} data={movie} {...movie} bg={bg} />
       ))}
